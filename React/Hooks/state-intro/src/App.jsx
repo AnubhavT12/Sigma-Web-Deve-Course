0acea4b773bd5a7246da1d0f2ead408a7890f3cc
Anubhav Thakur <anybhav12@gmail.com>
2026-07-30 22:12:30 +0530

    hooks in react

---
+++ React/Hooks/state-intro/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(10)
+
+  return (
+    <>
+     <div>the Count Is {count}</div>
+     <button onClick={()=>{setCount(count+1)}}>Update Count</button>
+
+    </>
+  )
+}
+
+export default App
